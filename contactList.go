be73598a6016544b6075638099b0248932475295
Topexan <54759948+Topexan@users.noreply.github.com>
2021-04-12 17:37:24 +0600

    updated getPerson, create and delete functions

--- contactList.go
+++ contactList.go
@@ -29,11 +29,11 @@ type Person struct {
 	Phone   string `json:"phone"`
 }
 
-var persons []Person
 //function to show all persons
 func getPersons(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
+
+	var persons []Person
 	db := setupDB()
 	defer db.Close()
 
@@ -71,11 +71,12 @@ func getPersons(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(persons)
 }
+
 //function to show one person
 func getPerson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) //read parameter from url
-	
+
 	db := setupDB()
 	defer db.Close()
 
@@ -106,13 +107,14 @@ func getPerson(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	json.NewEncoder(w).Encode(p)
 }
+
 //function to create new person
-func createPerson(w http.ResponseWriter, r *http.Request) { 
+func createPerson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
+
 	decoder := json.NewDecoder(r.Body)
 	var p Person //new Person
 	err := decoder.Decode(&p)
@@ -122,10 +124,10 @@ func createPerson(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = json.NewDecoder(r.Body).Decode(&p)
-	
-	p.ID = strconv.Itoa(rand.Intn(30 - 4) + 4) // random number from 4 to 30
+
+	p.ID = strconv.Itoa(rand.Intn(30-4) + 4) // random number from 4 to 30
 	//persons = append(persons, p) //adding new person to array
-	json.NewEncoder(w).Encode(p) 
+	json.NewEncoder(w).Encode(p)
 
 	db := setupDB()
 	defer db.Close()
@@ -133,16 +135,17 @@ func createPerson(w http.ResponseWriter, r *http.Request) {
 	sqlStatement := `
 		INSERT INTO persons (id, name, surname, city, phone)
 		VALUES ($1, $2, $3, $4, $5)`
-		_, err = db.Exec(sqlStatement, p.ID, p.Name, p.Surname, p.City, p.Phone)//add person to database
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("insertion comleted")
-} 
+	_, err = db.Exec(sqlStatement, p.ID, p.Name, p.Surname, p.City, p.Phone) //add person to database
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("insertion comleted")
+}
+
 // function to update existing person
-func updatePerson(w http.ResponseWriter, r *http.Request) { 
+func updatePerson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
+
 	decoder := json.NewDecoder(r.Body)
 	var p Person //person with new parameters
 	err := decoder.Decode(&p)
@@ -151,14 +154,7 @@ func updatePerson(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	for i := range persons {
-		if persons[i].ID == p.ID {
-			persons[i] = p //switch old person to new
-			json.NewEncoder(w).Encode(persons[i])
-			return
-		}
-	}
-	//json.NewEncoder(w).Encode(persons)
+	json.NewEncoder(w).Encode(p)
 
 	db := setupDB()
 	defer db.Close()
@@ -169,74 +165,33 @@ func updatePerson(w http.ResponseWriter, r *http.Request) {
 						   city = $4,
 						   phone = $5
 		WHERE id = $1;`
-		_, err = db.Exec(sqlStatement, p.ID, p.Name, p.Surname, p.City, p.Phone)//update person in our database
-		if err != nil {
-			panic(err)
-		}
+	_, err = db.Exec(sqlStatement, p.ID, p.Name, p.Surname, p.City, p.Phone) //update person in our database
+	if err != nil {
+		panic(err)
+	}
 
 }
+
 //function to delete person
-func deletePerson(w http.ResponseWriter, r *http.Request) { 
+func deletePerson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)//take id from url
-	for index, item := range persons {
-		if item.ID == params["id"] { //look for person with taken id and delete it from array
-			persons = append(persons[:index], persons[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(persons)
+	params := mux.Vars(r) //take id from url
 
 	db := setupDB()
 	defer db.Close()
 
 	sqlStatement := `
 		DELETE FROM persons
-		WHERE name = $1;`
-		_, err := db.Exec(sqlStatement, params["id"]) //delete person from database
-		if err != nil {
-			panic(err)
-		}
+		WHERE id = $1;`
+	_, err := db.Exec(sqlStatement, params["id"]) //delete person from database
+	if err != nil {
+		panic(err)
+	}
+	getPersons(w, r)
 }
-//function to take persons from database, and put persons to array
-func connectToDb() { 
-	/* db := setupDB()
-	defer db.Close()
 
-	err := db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-
-	rows, err := db.Query("SELECT id, name, surname, city, phone FROM persons")
-	if err != nil {
-		// handle this error better than this
-		panic(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id string
-		var name string
-		var surname string
-		var city string
-		var phone string
-		err = rows.Scan(&id, &name, &surname, &city, &phone)
-		if err != nil {
-			// handle this error
-			panic(err)
-		}
-		persons = append(persons, Person{ID: id, Name: name, Surname: surname, City: city, Phone: phone})
-	}
-	// get any error encountered during iteration
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	} */
-}
 //function to connect to database
-func setupDB() *sql.DB{ 
+func setupDB() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -249,7 +204,6 @@ func setupDB() *sql.DB{
 }
 
 func main() {
-	connectToDb() //connect to database and create array of persons
 	r := mux.NewRouter() //create new router
 	r.HandleFunc("/persons", getPersons).Methods("GET")
 	r.HandleFunc("/persons/{id}", getPerson).Methods("GET")
